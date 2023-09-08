adf4a43a412ae08d5cf833319724b2cba59cda64
phi_np <phi_np@dev.anybim.vn>
2023-09-08 11:44:38 +0700

    test deploy var env

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL, {
+    .connect('mongodb+srv://phinp2711:HZA7HT5hBPGLx5Tq@kaito.babv7rl.mongodb.net/joke', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
