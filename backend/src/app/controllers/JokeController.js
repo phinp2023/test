6584a77019778ed701984f479d57e0cdcc9306f5
phi_np <phi_np@dev.anybim.vn>
2023-09-08 16:47:38 +0700

    setting option cookie

--- backend/src/app/controllers/JokeController.js
+++ backend/src/app/controllers/JokeController.js
@@ -12,9 +12,10 @@ class JokeController {
 
             const options = {
                 expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-                httpOnly: true,
+                httpOnly: false,
                 sameSite: 'none',
                 secure: true,
+                domain: 'joke-frontend.vercel.app',
             };
 
             if (!id_user) {
